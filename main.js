6aad9d5b783d47f5baa0fc9582f28a208b4f3b6d
matthewglennpickering <matthewglennpickering@gmail.com>
2022-12-13 07:53:02 -0800

    index.html

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
 
 let amount = size * size;
-for (let i = 0; i <amount; i++) {
+for (let i = 0; i < amount; i++) {
     let square = document.createElement('div');
     square.addEventListener('mouseover', colorSquare);
     square.style.backgroundColor = 'white';
@@ -22,15 +22,28 @@ for (let i = 0; i <amount; i++) {
 populateBoard(16);
 
 function changeSize(input){
-    if(input >=2 && input <=100) {
+ if(input >=2 && input <=100) {
     populateBoard(input);
-    }
-    else {
+    } else {
         console.log('too many squares');
     }
     
 }
 
 function colorSquare(){
-    this.style.backgroundColor = color;
+    if(color === "random"){
+        this.style.backgroundColor = `hs1(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetBoard(){
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = "white");
 }
